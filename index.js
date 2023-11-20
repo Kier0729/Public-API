f19da76a588cc70eddcc43445d5b1fd86e036a1c
Kier Dalit <kier0729@gmail.com>
2023-11-20 23:21:28 +0800

    Create next and prev button for home/proplayer page

--- index.js
+++ index.js
@@ -5,47 +5,80 @@ const app = express();
 const port = 3000;
 const API_URL = "https://api.opendota.com/api";
 var currYear = "";
+var x = "";
+var result;
+var homePro;
+
 app.use(express.static("public"));
 
 // Making a custom middleware(currentYear) to get the current year
 function currentYear(req, res, next){
     const year = new Date();
     currYear = year.getFullYear();
+    
     next();
 }
 
 // mounting/using the currentYear middleware
 app.use(currentYear);
 
+// displays set of 15 pro players
 app.get("/", async (req, res) => {
 
-    
+    x = 0;
         try{
-            const result = await axios.get(API_URL+"/proPlayers");
-            const homePro = result.data;
-            res.render("index.ejs",{
+        result = await axios.get(API_URL+"/proPlayers");
+        homePro = result.data;
+        
+                res.render("index.ejs",{
                 currYear,
-                homePro
+                homePro,
+                x
             });
-            
+               
         } catch (error) {
             console.log(error.message);
-        }
-        
-        
-        /*, {
-            secret: JSON.stringify(result.data.secret),
-            user: JSON.stringify(result.data.username)
-        }*/
-        // try {
-        //     const result = await axios.get(API_URL);
-        // } catch (error) {
-        // res.render("index.ejs"/*, {
-        //     secret: JSON.stringify(error.message)
-        // }*/);
-        // }
-      
+        }     
   });
+
+// display the next 15 pro players
+  app.get("/next", async (req, res) => {
+    x = x + 15;
+    try{
+        result = await axios.get(API_URL+"/proPlayers");
+        homePro = result.data;
+        res.render("index.ejs",{
+            currYear,
+            homePro,
+            x
+        });
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+// displays the previous 15 pro players
+app.get("/prev", async (req, res) => {
+    if (x >= 15){
+    x = x - 15;
+    try{
+        result = await axios.get(API_URL+"/proPlayers");
+        homePro = result.data;
+        res.render("index.ejs",{
+            currYear,
+            homePro,
+            x
+        });
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+} else {
+    res.redirect("/");
+}
+
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
